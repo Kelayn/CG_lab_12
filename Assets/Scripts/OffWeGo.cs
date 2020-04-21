7407131fa68b0a00bf646e3d4ed3ccaae161d40c
Andrew <andrey.arakelyan@bask.ws>
2020-04-21 03:29:47 +0300

    first commit

---
+++ Assets/Scripts/OffWeGo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffWeGo : MonoBehaviour
+{
+
+    public GameObject gObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gObj.GetComponent<Renderer>().enabled = !gObj.GetComponent<Renderer>().enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    Animator anim;
+    bool isCollided = false;
+    bool isOpened = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isCollided && !isOpened)
+        {
+            Debug.Log("OPEN");
+            anim.SetTrigger("DoorOpenTrigger");  
+        }
+
+        if (Input.GetKeyUp(KeyCode.E) && isCollided)
+        {
+            anim.ResetTrigger("DoorCloseTrigger");
+            anim.ResetTrigger("DoorOpenTrigger");
+            isOpened = !isOpened;
+
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.E) && isCollided && isOpened)
+        {
+            Debug.Log("CLOSE");
+            anim.SetTrigger("DoorCloseTrigger");  
+       }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+       isCollided = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isCollided = false;
+    }
+}
